6a1ac3f79e93893aaa634fd6276c84e503ae9b70
Andrea Lee <alee9707@usc.edu>
2022-11-03 15:11:06 -0700

    Added Firebase Auth and Database to our application

--- app/src/main/java/com/example/socalbeachesforlife/RegisterUser.java
+++ app/src/main/java/com/example/socalbeachesforlife/RegisterUser.java
@@ -1,14 +1,140 @@
 package com.example.socalbeachesforlife;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegisterUser extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
+public class RegisterUser extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView banner, registerUser;
+    private EditText editTextFullName, editTextEmail,
+            editTextPassword, editTextVerifyPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.register_button);
+        registerUser.setOnClickListener(this);
+
+        editTextFullName = (EditText) findViewById(R.id.full_name);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+        editTextVerifyPassword = (EditText) findViewById(R.id.verify_password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.register_button:
+                registerUser();
+                break;
+
+        }
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String fullName = editTextFullName.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String verifyPassword = editTextVerifyPassword.getText().toString().trim();
+
+        if(fullName.isEmpty()) {
+            editTextFullName.setError("Full Name is Required!");
+            editTextFullName.requestFocus();
+            return;
+        }
+        if(email.isEmpty()) {
+            editTextEmail.setError("Email is Required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please provide valid email.");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()) {
+            editTextPassword.setError("Password is Required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+        else if(password.length() < 6) {
+            editTextPassword.setError("Please set password longer than 6 characters.");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(verifyPassword.isEmpty()) {
+            editTextVerifyPassword.setError("Verify Password is Required!");
+            editTextVerifyPassword.requestFocus();
+            return;
+        }
+        else if(password.compareTo(verifyPassword) != 0) {
+            editTextVerifyPassword.setError("Passwords do not match!");
+            editTextVerifyPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            User user = new User(fullName,email);
+                            DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("Users");
+                            String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                            myRef.child(userID).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()) {
+                                        Toast.makeText(RegisterUser.this, "User has been registered successfully!", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.VISIBLE);
+                                    }
+                                    else {
+                                        Toast.makeText(RegisterUser.this, "Failed to register! Try Again! Else After", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(RegisterUser.this, "Failed to register! Try Again!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/example/socalbeachesforlife/User.java
@@ -0,0 +1,13 @@
+package com.example.socalbeachesforlife;
+
+public class User {
+    private String fullName, email;
+
+    public User() {
+
+    }
+    public User(String fullName, String email) {
+        this.fullName = fullName;
+        this.email = email;
+    }
+}
