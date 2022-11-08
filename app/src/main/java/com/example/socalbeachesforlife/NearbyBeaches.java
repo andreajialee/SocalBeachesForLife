4c120c97322daa06aeca121a9a5895be0e87b40d
Andrew Larratt <larratt@usc.edu>
2022-11-07 16:49:16 -0800

    Restaurants now show up on the map. When there is none around then none show up.

--- app/src/main/java/com/example/socalbeachesforlife/NearbyBeaches.java
+++ app/src/main/java/com/example/socalbeachesforlife/NearbyBeaches.java
@@ -2,13 +2,20 @@ package com.example.socalbeachesforlife;
 
 import static com.example.socalbeachesforlife.BuildConfig.MAPS_API_KEY;
 
+import android.graphics.Color;
 import android.location.Location;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
 
 import com.google.android.gms.common.util.ArrayUtils;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -20,6 +27,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class NearbyBeaches extends AsyncTask<Object, String, String> {
     public static double ETA;
     private String googlePlacesData;
@@ -52,15 +60,11 @@ public class NearbyBeaches extends AsyncTask<Object, String, String> {
         showNearbyPlaces(nearbyPlaceList);
     }
 
-    private void showNearbyPlaces(List<HashMap<String, String>> nearbyPlaceList)
-    {
+    private void showNearbyPlaces(List<HashMap<String, String>> nearbyPlaceList) {
         int count = 0;
-        for(int i = 0; i < nearbyPlaceList.size(); i++)
-        {
-            MarkerOptions markerOptions = new MarkerOptions();
+        for (int i = 0; i < nearbyPlaceList.size(); i++) {
             HashMap<String, String> googlePlace = nearbyPlaceList.get(i);
             String placeName = googlePlace.get("place_name");
-            String vicinity = googlePlace.get("vicinity");
             double lat = Double.parseDouble(googlePlace.get("lat"));
             double lng = Double.parseDouble(googlePlace.get("lng"));
 
@@ -77,36 +81,53 @@ public class NearbyBeaches extends AsyncTask<Object, String, String> {
             RouteGetter routeGetter = new RouteGetter();
             routeGetter.execute(dataTransfer);
 
-            if(count < 5) {
+            if (count < 5) {
                 likelyPlaceLatLngs[count] = latLng;
                 likelyPlaceNames[count] = placeName;
                 likelyPlaceETA[count] = ETA;
                 count++;
-            }
-            else {
+            } else {
                 double temp = 0;
                 int k = 0;
-                for(int j = 0; j < 5; j++) {
-                    if(likelyPlaceETA[j] < temp) {
+                for (int j = 0; j < 5; j++) {
+                    if (likelyPlaceETA[j] < temp) {
                         temp = likelyPlaceETA[j];
                         k = j;
                     }
                 }
-                if(likelyPlaceETA[k] > ETA) {
+                if (likelyPlaceETA[k] > ETA) {
                     likelyPlaceLatLngs[k] = latLng;
                     likelyPlaceNames[k] = placeName;
                     likelyPlaceETA[k] = ETA;
                 }
             }
-            markerOptions.position(latLng);
-            markerOptions.title(placeName + " : " + vicinity);
-            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-            mMap.addMarker(markerOptions);
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
         }
         MapsActivity.likelyPlaceNames = likelyPlaceNames;
         MapsActivity.likelyPlaceLatLngs = likelyPlaceLatLngs;
+        for (int i = 0; i < 5; i++) {
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(likelyPlaceLatLngs[i]);
+            markerOptions.title(likelyPlaceNames[i]);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+            mMap.addMarker(markerOptions);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(likelyPlaceLatLngs[i]));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                Circle circle = mMap.addCircle(new CircleOptions()
+                        .center(likelyPlaceLatLngs[i])
+                        .radius(3000)
+                        .strokeColor(Color.RED)
+                        .fillColor(Color.argb(.25f, 0f, 0f, 1f)));
+            }
+            Object dataTransfer[] = new Object[2];
+            NearbyResaurants nearbyResaurants = new NearbyResaurants();
+            int radius = 2000;
+            String reurl = getRestaurantUrl(likelyPlaceLatLngs[i].latitude, likelyPlaceLatLngs[i].longitude, radius);
+            dataTransfer[0] = mMap;
+            dataTransfer[1] = reurl;
+
+            nearbyResaurants.execute(dataTransfer);
+        }
     }
 
     private String getUrl(double olatitude, double olongitude, double lat, double lng)
@@ -114,6 +135,17 @@ public class NearbyBeaches extends AsyncTask<Object, String, String> {
         StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/directions/json?");
         googlePlaceUrl.append("origin="+olatitude+","+olongitude);
         googlePlaceUrl.append("&destination="+lat+","+lng);
+        googlePlaceUrl.append("&key="+MAPS_API_KEY);
+
+        return googlePlaceUrl.toString();
+    }
+
+    private String getRestaurantUrl(double latitude, double longitude, int radius)
+    {
+        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlaceUrl.append("location="+latitude+","+longitude);
+        googlePlaceUrl.append("&radius="+radius);
+        googlePlaceUrl.append("&type=restaurant");
         googlePlaceUrl.append("&key="+MAPS_API_KEY);
 
         return googlePlaceUrl.toString();
---
+++ app/src/main/java/com/example/socalbeachesforlife/RestaurantParser.java
@@ -0,0 +1,77 @@
+package com.example.socalbeachesforlife;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class RestaurantParser {
+    private HashMap<String, String> getPlace(JSONObject googlePlaceJson)
+    {
+        HashMap<String, String> googlePlaceMap = new HashMap<>();
+        String latitude = "";
+        String longitude = "";
+        String name = "";
+        String open = "--NA--";
+        String rating = "--NA--";
+
+        try {
+            latitude = googlePlaceJson.getJSONObject("geometry").getJSONObject("location").getString("lat");
+            longitude = googlePlaceJson.getJSONObject("geometry").getJSONObject("location").getString("lng");
+
+            if(!googlePlaceJson.isNull("rating")) {
+                rating = googlePlaceJson.getString("rating");
+            }
+            if(!googlePlaceJson.isNull("opening_hours") && !googlePlaceJson.getJSONObject("opening_hours").isNull("open_now")) {
+                open = googlePlaceJson.getJSONObject("opening_hours").getString("open_now");
+            }
+
+            name = googlePlaceJson.getString("name");
+
+            googlePlaceMap.put("lat", latitude);
+            googlePlaceMap.put("lng", longitude);
+            googlePlaceMap.put("name", name);
+            googlePlaceMap.put("open", open);
+            googlePlaceMap.put("rating", rating);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return googlePlaceMap;
+    }
+
+    private List<HashMap<String, String>> getPlaces(JSONArray jsonArray)
+    {
+        int count = jsonArray.length();
+        List<HashMap<String, String>> placelist = new ArrayList<>();
+        HashMap<String, String> placeMap = null;
+
+        for(int i = 0; i < count; i++) {
+            try {
+                placeMap = getPlace((JSONObject) jsonArray.get(i));
+                placelist.add(placeMap);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return placelist;
+    }
+
+    public List<HashMap<String, String>> parse(String jsonData)
+    {
+        JSONArray jsonArray = null;
+        JSONObject jsonObject;
+
+        try {
+            jsonObject = new JSONObject(jsonData);
+            jsonArray = jsonObject.getJSONArray("results");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return getPlaces(jsonArray);
+    }
+}
