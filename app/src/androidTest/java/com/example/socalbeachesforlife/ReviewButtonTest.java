61426c00f66a01d82106f86d7b968f55f16df61e
Naomi Ramos Benitez <ramosben@usc.edu>
2022-11-21 17:48:23 -0800

    Added espresso black box tests

--- app/src/androidTest/java/com/example/socalbeachesforlife/ReviewButtonTest.java
+++ app/src/androidTest/java/com/example/socalbeachesforlife/ReviewButtonTest.java
@@ -15,6 +15,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
